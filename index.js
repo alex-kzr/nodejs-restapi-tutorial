43ae9ec1d858a98bca8ab643b9edde370de647ca
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-20 19:47:38 +1000

    Add json content type to headers

--- index.js
+++ index.js
@@ -61,6 +61,7 @@
             var payloadString = JSON.stringify(payload);
 
             // Return the response
+            res.setHeader('Content-Type', 'application/json');
             res.writeHead(statusCode);
             res.end(payloadString);
 
