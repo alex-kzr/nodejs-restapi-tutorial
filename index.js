ff87b7b3069070200a4598b5b959fb8a2208f821
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-20 22:08:28 +1000

    Add https support

--- index.js
+++ index.js
@@ -3,15 +3,41 @@
  *
  */
 
- // Dependencies
- var http = require('http');
- var url = require('url');
- var StringDecoder = require('string_decoder').StringDecoder;
- var config = require('./config');
+// Dependencies
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
+var config = require('./config');
 
- // The server should respond to all requests with a string
- var server = http.createServer(function(req, res){
-    
+// Instatiate the HTTP server
+var httpServer = http.createServer(function(req, res){
+    unifiedServer(req,res);    
+});
+
+// Start the HTTP server
+httpServer.listen(config.httpPort, function(){
+    console.log("The server is listening on http port "+config.httpPort+" in "+config.envName+" mode");
+});
+
+// Instatiate the HTTPS server
+var httpsServerOptions = {
+    'key' : fs.readFileSync('./https/key.pem'),
+    'cert' : fs.readFileSync('./https/cert.pem')
+};
+var httpsServer = http.createServer(httpsServerOptions, function(req, res){
+    unifiedServer(req,res);    
+});
+
+// Start the HTTPS server
+httpsServer.listen(config.httpsPort, function(){
+    console.log("The server is listening on https port "+config.httpsPort+" in "+config.envName+" mode");
+});
+
+// All the server logic for both http and https server
+var unifiedServer = function(req, res){
+
     // Get the URL and parse it
     var parsedUrl = url.parse(req.url, true);
 
@@ -70,29 +96,24 @@
             console.log('Returning this response: ', statusCode, payloadString);
         });
     });
- });
+};
 
- // Start the server
- server.listen(config.port, function(){
-    console.log("The server is listening on port "+config.port+" in "+config.envName+" mode");
- });
+// Define handlers
+var handlers = {};
 
- // Define handlers
- var handlers = {};
-
- // Sample handler
- handlers.sample = function(data, callback){
+// Sample handler
+handlers.sample = function(data, callback){
     
     // Callback a http status code and a payload object
     callback(406, {'name' : 'sample handler'});
- };
+};
 
- // Not found handler
- handlers.notFound = function(data, callback){
+// Not found handler
+handlers.notFound = function(data, callback){
     callback(404);
 };
  
- // Define a request router
- var router = {
+// Define a request router
+var router = {
     'sample' : handlers.sample
- };
+};
